90f6ee00859528f3c8fd0360cdfe06e9ac54cef8
tge <tgeconf@gmail.com>
2020-06-03 10:25:57 +0800

    dsvg

---
+++ dsvg/precache-manifest.22f87d7d81c59a82601a29d6e75ae395.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e145f28fab78b1f8acc2a7d27e2c2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d71b268eff1c62760b4f",
+    "url": "/static/css/2.ec2cc064.chunk.css"
+  },
+  {
+    "revision": "3474414353aa6daec22d",
+    "url": "/static/css/main.cedc937e.chunk.css"
+  },
+  {
+    "revision": "d71b268eff1c62760b4f",
+    "url": "/static/js/2.3bf344bd.chunk.js"
+  },
+  {
+    "revision": "3474414353aa6daec22d",
+    "url": "/static/js/main.0065d1c6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d961fdfabbe512d2675de09af09f598b",
+    "url": "/static/media/jsoneditor-icons.d961fdfa.svg"
+  }
+]);
